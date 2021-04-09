b4dfd876dfa7c850f592356376a84aa423960e2f
Administrator <Administrator@PC-20190705JSMM>
2021-04-09 12:50:40 +0800

    homework

---
+++ Homework6/OrderDemo/OrderDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OrderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderService os = new OrderService();
+            bool flag = true;
+            while (flag)
+            {
+                ShowMenu();
+                string chosen = Console.ReadLine();
+                switch (chosen)
+                {
+                    case "1": os.AddOrder(); break;
+                    case "2": os.DelOrder(); break;
+                    case "3": {
+                            Console.WriteLine("输入1根据订单金额查询订单，输入2根据客户名查询订单");
+                            int i = Convert.ToInt32(Console.ReadLine());
+                            os.SearchOrder(i);break;
+                        }
+                    case "4": os.ShowOrder(); break;
+                    case "5": os.order.Sort(); break;
+                    case "6": os.Export(); break;
+                    case "7": os.Import();break;
+                    case "8": flag = false; break;
+                    default: Console.WriteLine("输入错误"); break;
+                }
+            }
+        }
+
+        static void ShowMenu() {
+            Console.WriteLine("输入1增加订单");
+            Console.WriteLine("输入2删除订单");
+            Console.WriteLine("输入3查询订单");
+            Console.WriteLine("输入4显示所有订单");
+            Console.WriteLine("输入5根据订单号为订单排序");
+            Console.WriteLine("输入6将订单序列化");
+            Console.WriteLine("输入7将订单反序列化");
+            Console.WriteLine("输入8退出");
+        }
+    }
+}
---
+++ Homework6/OrderDemo/OrderDemoTests/OrderServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderDemo.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void ShowOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DelOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
