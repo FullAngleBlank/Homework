e07fd77144ecdebccbdba51343822d49eaac549c
Administrator <Administrator@PC-20190705JSMM>
2021-04-15 21:40:44 +0800

    homework

---
+++ Homework7/CayleyTree/CayleyTree/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace CayleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.clear = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tb6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tb5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.draw = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 515);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.clear);
+            this.panel3.Controls.Add(this.save);
+            this.panel3.Controls.Add(this.cb);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.tb6);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.tb5);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.tb4);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.tb2);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.tb3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.tb1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 106);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 409);
+            this.panel3.TabIndex = 1;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(110, 369);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 16;
+            this.clear.Text = "清空";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(18, 369);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 15;
+            this.save.Text = "保存";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // cb
+            // 
+            this.cb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb.FormattingEnabled = true;
+            this.cb.Items.AddRange(new object[] {
+            "黑色",
+            "蓝色",
+            "黄色",
+            "绿色"});
+            this.cb.Location = new System.Drawing.Point(94, 324);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(100, 23);
+            this.cb.TabIndex = 14;
+            this.cb.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.Location = new System.Drawing.Point(6, 323);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 23);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "画笔颜色：";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb6
+            // 
+            this.tb6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb6.Location = new System.Drawing.Point(94, 280);
+            this.tb6.Name = "tb6";
+            this.tb6.Size = new System.Drawing.Size(100, 25);
+            this.tb6.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.Location = new System.Drawing.Point(6, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 23);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "左角度：";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb5
+            // 
+            this.tb5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb5.Location = new System.Drawing.Point(94, 233);
+            this.tb5.Name = "tb5";
+            this.tb5.Size = new System.Drawing.Size(100, 25);
+            this.tb5.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.Location = new System.Drawing.Point(6, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "右角度：";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb4
+            // 
+            this.tb4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb4.Location = new System.Drawing.Point(94, 186);
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(100, 25);
+            this.tb4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.Location = new System.Drawing.Point(6, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "左长度比：";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb2
+            // 
+            this.tb2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb2.Location = new System.Drawing.Point(94, 95);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(100, 25);
+            this.tb2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Location = new System.Drawing.Point(6, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "主干长度：";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb3
+            // 
+            this.tb3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb3.Location = new System.Drawing.Point(94, 139);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(100, 25);
+            this.tb3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Location = new System.Drawing.Point(3, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "右长度比：";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(15, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "请输入cayley参数：";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tb1
+            // 
+            this.tb1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb1.Location = new System.Drawing.Point(94, 48);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(100, 25);
+            this.tb1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "递归深度：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 106);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 27F);
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(28, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cayley";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.draw);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(200, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(541, 515);
+            this.panel4.TabIndex = 1;
+            // 
+            // draw
+            // 
+            this.draw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.draw.Location = new System.Drawing.Point(445, 12);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(84, 35);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "点击生成";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 515);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tb6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tb5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button draw;
+    }
+}
+
---
+++ Homework7/CayleyTree/CayleyTree/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CayleyTree
+{
+    public partial class Form1 : Form
+    {
+        private int n;
+        private double leng;
+        private double per1;
+        private double per2;
+        private double th1;
+        private double th2;
+        private Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cb.SelectedIndex = 0;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            this.n = Convert.ToInt32(tb1.Text);
+            this.leng = Convert.ToDouble(tb2.Text);
+            this.per1 = Convert.ToDouble(tb3.Text);
+            this.per2 = Convert.ToDouble(tb4.Text);
+            this.th1 = Convert.ToDouble(tb5.Text) * Math.PI/180;
+            this.th2 = Convert.ToDouble(tb6.Text) * Math.PI/180;
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            tb1.Text = "";
+            tb2.Text = "";
+            tb3.Text = "";
+            tb4.Text = "";
+            tb5.Text = "";
+            tb6.Text = "";
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            if (graphics == null)
+                graphics = panel4.CreateGraphics();
+            DrawCrayleyTree(n, 200, 310, leng, -Math.PI / 2);
+            
+        }
+
+
+        private Graphics graphics;
+        
+        void DrawCrayleyTree(int n,double x0, double y0, double leng, double th)
+        {
+            if (n == 0)return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            MyDrawLine(x0, y0, x1, y1);
+
+            DrawCrayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            DrawCrayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+
+        void MyDrawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            switch (cb.SelectedIndex)
+            {
+                case 0:
+                    pen = Pens.Black;
+                    break;
+                case 1:
+                    pen = Pens.Blue;
+                    break;
+                case 2:
+                    pen = Pens.Yellow;
+                    break;
+                case 3:
+                    pen = Pens.Green;
+                    break;
+                default:
+                    pen = Pens.Black;
+                    break;
+            }
+        }
+    }
+}
